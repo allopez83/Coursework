07d264f78f78946be4d1402032b61d52889b6d4a
Hansen Wu <hansen.wu.85@gmail.com>
2015-04-06 10:22:01 -0700

    Added old gui assignment It was untracked for some reason

---
+++ CS1B/Homework/assignment_09/export/Main.java
@@ -0,0 +1,18 @@
+package assignment_09.export;
+
+/**
+ * This class contains the main to initialize ConverterApp, a temperature
+ * conversion program. ConverterApp uses a GUI.
+ * 
+ * @author Hansen Wu
+ * 
+ */
+public class Main {
+	public static void main(String[] args) {
+		new ConverterApp();
+	}
+
+}
+
+/************PASTE FROM CONSOLE************
+**************END OF PASTE*****************/
