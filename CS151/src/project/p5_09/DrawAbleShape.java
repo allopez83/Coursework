e49704b45f5e53b14b3bf728eb566a7962a728b9
hansenwu <hansen.wu.85@gmail.com>
2015-05-09 17:40:15 -0700

    Changing styles also changes background color written by me cs151 project

---
+++ CS151/src/project/p5_09/DrawAbleShape.java
@@ -0,0 +1,14 @@
+package project.p5_09;
+
+import java.awt.Graphics2D;
+/**
+ * Interface that describes a drawable shape. Any shape that implements this
+ * interface must provide the draw method
+ * 
+ * @author Jordan Petersen,(Ryan) Tuan Phan, Dustin Tran, Hansen Wu
+ * @version 05/09/15
+ */
+
+public interface DrawAbleShape {
+	void draw (Graphics2D g2);
+}
