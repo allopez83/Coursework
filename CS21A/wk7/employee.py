cd99afeaf11de6505056f0815ffc22e56c9bccec
hansenwu <hansen.wu.85@gmail.com>
2016-02-24 02:23:35 -0800

    Completed wk7 homework

--- CS21A/wk7/employee.py
+++ CS21A/wk7/employee.py
@@ -1,6 +1,6 @@
 """ An employee object represents an employee at company, with a first name, last name, social security number, and salary
 """
-class employee:
+class Employee:
     """ Create an employee with the given firstName and lastName string, social security number int as ssn, and salary double
     """
     def __init__(self, firstName, lastName, ssn, salary):
@@ -8,15 +8,19 @@ class employee:
         self.lastName = lastName
         self.ssn = ssn
         self.salary = salary
+
     """ Represents an employee object in string form, with first and last name, ssn, and salary, in that order.
     """
     def __str__(self):
         return self.firstName + " " + self.lastName + ", ssn: " + str(self.ssn) + ", salary: " + str(self.salary)
+
     """ Increases an employee's salary by a specified percentage, where 0.25 is 25%
     """
     def giveRaise(self, percentRaise):
         self.salary += self.salary * percentRaise
 
 if __name__ == "__main__":
-    e1 = employee("Adam", "Last", 1111, 60000)
+    e1 = Employee("Adam", "Last", 1111, 60000)
+    print(e1)
+    e1.giveRaise(.2)
     print(e1)
--- CS21A/wk7/manager.py
+++ CS21A/wk7/manager.py
@@ -1,17 +1,23 @@
-""" One object of class SavingsAccount represents one
-savings account with a customer, a balance, and an interest rate
+""" Manager object represents a manager, subclass of employee, with added title and bonus
 """
 
 from employee import Employee
 
-# class Manager (Employee):
-#
-#    def __init__(self):
-#
-#    def __str__(self):
-#
-# if __name__ == "__main__":
-#     account = Account()
-#     print (account)
-#     savingsAccount = SavingsAccount()
-#     print (savingsAccount)
+class Manager(Employee):
+    """ Create manager object, requiring first and last name, ssn, and salary for employee parent class, as well as string title for manager's title and double bonus for the annual bonus received.
+    """
+    def __init__(self, firstName, lastName, ssn, salary, title, bonus):
+        Employee.__init__(self, firstName, lastName, ssn, salary)
+        self.title = title
+        self.bonus = bonus
+
+    """ Represents a manager object in string form, with first and last name, ssn, salary, title, and bonus
+    """
+    def __str__(self):
+        return Employee.__str__(self) + ", title: " + self.title + ", bonus: " + str(self.bonus)
+
+if __name__ == "__main__":
+    m1 = Manager("Bob", "Last", 2222, 100000, "Project Lead", 5000)
+    print(m1)
+    m1.giveRaise(.2)
+    print(m1)
