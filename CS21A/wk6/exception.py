06066be23de8c00722da34e735f78bcb29b07ec9
hansenwu <hansen.wu.85@gmail.com>
2016-02-24 01:38:26 -0800

    Started wk7

--- CS21A/wk6/exception.py
+++ CS21A/wk6/exception.py
@@ -1,15 +1,15 @@
 import random
 
-secretNumber = random.randint(1,10)
-userInput = input ("Pick a number ")
+secretNumber = random.randint(1, 10)
+userInput = input("Pick a number ")
 try:
     userGuess = int(userInput)
-except:              # executed only when an Error IS raised
-    print ("you must type a number") 
-else:                # executed only when an Error IS NOT raised
-    if userGuess == secretNumber:    
-        print ("You got it!")
+except:  # executed only when an Error IS raised
+    print("you must type a number")
+else:  # executed only when an Error IS NOT raised
+    if userGuess == secretNumber:
+        print("You got it!")
     else:
-        print ("Wrong, the number is "+ str(secretNumber))
+        print("Wrong, the number is " + str(secretNumber))
 finally:
-   print ("goodbye")
+    print("goodbye")
---
+++ CS21A/wk7/manager.py
@@ -0,0 +1,17 @@
+""" One object of class SavingsAccount represents one
+savings account with a customer, a balance, and an interest rate
+"""
+
+from employee import Employee
+
+# class Manager (Employee):
+#
+#    def __init__(self):
+#
+#    def __str__(self):
+#
+# if __name__ == "__main__":
+#     account = Account()
+#     print (account)
+#     savingsAccount = SavingsAccount()
+#     print (savingsAccount)
