539b434c22af07b0054d7a5f8b946bb595dc864f
hansenwu <hansen.wu.85@gmail.com>
2016-09-28 20:05:22 -0700

    Completed lab08 functions, added details on project

--- CS152/lab08/functional.js
+++ CS152/lab08/functional.js
@@ -1,23 +1,26 @@
 var foldl = function (f, acc, array) {
+    for (var i = 0; i < array.length; i++)
+        acc = f(array[i], acc)
+    return acc
 }
 
-console.log(foldl(function(x,y){return x+y}, 0, [1,2,3]));
+// console.log(foldl(function(x,y){return x+y}, 0, [1,2,3]));
 
 var foldr = function (f, z, array) {
+    // console.log(array[array.length-1])
+    for (var i = array.length-1; i >= 0; i--) {
+        z = f(array[i], z)
+        // console.log(z)
+    }
+    return z
 }
 
-console.log(foldl(function(x,y){return x/y}, 1, [2,4,8]));
+// console.log(foldl(function(x,y){return x/y}, 1, [2,4,8]));
 
 var map = function (f, array) {
+    for (var i = 0; i < array.length; i++)
+        array[i] = f(array[i])
+    return array
 }
 
 console.log(map(function(x){return x+x}, [1,2,3,5,7,9,11,13]));
-
-
-// Write a curry function as we discussed in class.
-// Create a `double` method using the curry function
-// and the following `mult` function.
-function mult(x,y) {
-  return x * y;
-}
-
---
+++ CS152/project/part1/readme.md
@@ -0,0 +1,5 @@
+Project 1
+==============================
+
+- fwjs, feather weight javascript
+- similar to hw2
