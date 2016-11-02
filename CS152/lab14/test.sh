49cf6bfdef5b5467a1b2864937f6a531a80641a4
hansenwu <hansen.wu.85@gmail.com>
2016-11-02 16:06:18 -0700

    updated jar

--- CS152/lab14/test.sh
+++ CS152/lab14/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir out
-java -jar /usr/local/lib/antlr-4.4-complete.jar -no-listener -visitor Expr.g4 -o out/
+java -jar /usr/local/lib/antlr-4.5.3-complete.jar -no-listener -visitor Expr.g4 -o out/
 cp *.java out/
 javac out/*.java
 java -classpath .:out/:${CLASSPATH} Calc $1
